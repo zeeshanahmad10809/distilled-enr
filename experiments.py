4743147713e1295fe987d8a19130480f28101b40
Zeeshan Ahmad <zeeshanahmad10809@gmail.com>
2024-02-28 02:18:50 +0100

    changed experiments.py

--- experiments.py
+++ experiments.py
@@ -7,72 +7,91 @@ from misc.dataloaders import scene_render_dataloader
 from models.neural_renderer import NeuralRenderer
 from training.training import Trainer
 
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Get path to data from command line arguments
-if len(sys.argv) != 2:
-    raise(RuntimeError("Wrong arguments, use python experiments.py <config>"))
-path_to_config = sys.argv[1]
 
-# Open config file
-with open(path_to_config) as file:
-    config = json.load(file)
+def load_eg3d_model(network_path):
+    # if not torch.cuda.is_available():
+    #     raise AssertionError("gpu must be available to load eg3d!")
+    # eg3d_teacher = torch.load(network_path).to(device)
+    # return eg3d_teacher
+    return None
 
-# Set up directory to store experiments
-timestamp = time.strftime("%Y-%m-%d_%H-%M")
-directory = "{}_{}".format(timestamp, config["id"])
-if not os.path.exists(directory):
-    os.makedirs(directory)
 
-# Save config file in directory
-with open(directory + '/config.json', 'w') as file:
-    json.dump(config, file)
+def main ():
+    # Get path to data from command line arguments
+    if len(sys.argv) != 2:
+        raise(RuntimeError("Wrong arguments, use python experiments.py <config>"))
+    path_to_config = sys.argv[1]
 
-# Set up renderer
-model = NeuralRenderer(
-    img_shape=config["img_shape"],
-    channels_2d=config["channels_2d"],
-    strides_2d=config["strides_2d"],
-    channels_3d=config["channels_3d"],
-    strides_3d=config["strides_3d"],
-    num_channels_inv_projection=config["num_channels_inv_projection"],
-    num_channels_projection=config["num_channels_projection"],
-    mode=config["mode"]
-)
+    # Open config file
+    with open(path_to_config) as file:
+        config = json.load(file)
 
-model.print_model_info()
+    # Set up directory to store experiments
+    timestamp = time.strftime("%Y-%m-%d_%H-%M")
+    directory = "{}_{}".format(timestamp, config["id"])
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-model = model.to(device)
+    # Save config file in directory
+    with open(directory + '/config.json', 'w') as file:
+        json.dump(config, file)
 
-if config["multi_gpu"]:
-    model = torch.nn.DataParallel(model)
+    # load eg3d model
+    eg3d_teacher = load_eg3d_model(config["eg3d_model"])
 
-# Set up trainer for renderer
-trainer = Trainer(device, model, lr=config["lr"],
-                  rendering_loss_type=config["loss_type"],
-                  ssim_loss_weight=config["ssim_loss_weight"])
+    # Set up renderer
+    model = NeuralRenderer(
+        img_shape=config["img_shape"],
+        channels_2d=config["channels_2d"],
+        strides_2d=config["strides_2d"],
+        channels_3d=config["channels_3d"],
+        strides_3d=config["strides_3d"],
+        num_channels_inv_projection=config["num_channels_inv_projection"],
+        num_channels_projection=config["num_channels_projection"],
+        mode=config["mode"],
+        teacher_renderer=eg3d_teacher,
+    )
 
-dataloader = scene_render_dataloader(path_to_data=config["path_to_data"],
-                                     batch_size=config["batch_size"],
-                                     img_size=config["img_shape"],
-                                     crop_size=128)
+    # model.print_model_info()
+    model = model.to(device)
 
-# Optionally set up test_dataloader
-if config["path_to_test_data"]:
-    test_dataloader = scene_render_dataloader(path_to_data=config["path_to_test_data"],
-                                              batch_size=config["batch_size"],
-                                              img_size=config["img_shape"],
-                                              crop_size=128)
-else:
+    if config["multi_gpu"]:
+        model = torch.nn.DataParallel(model)
+
+    # Set up trainer for renderer
+    trainer = Trainer(device, model, lr=config["lr"],
+                      rendering_loss_type=config["loss_type"],
+                      ssim_loss_weight=config["ssim_loss_weight"])
+
+    dataloader = scene_render_dataloader(path_to_data=config["path_to_data"],
+                                         batch_size=config["batch_size"],
+                                         img_size=config["img_shape"],
+                                         crop_size=128)
+
+    # Optionally set up test_dataloader
+    # if config["path_to_test_data"]:
+    #     test_dataloader = scene_render_dataloader(path_to_data=config["path_to_test_data"],
+    #                                               batch_size=config["batch_size"],
+    #                                               img_size=config["img_shape"],
+    #                                               crop_size=128)
+    # else:
+    #     test_dataloader = None
     test_dataloader = None
 
-print("PID: {}".format(os.getpid()))
+    print("PID: {}".format(os.getpid()))
 
-# Train renderer, save generated images, losses and model
-trainer.train(dataloader, config["epochs"], save_dir=directory,
-              save_freq=config["save_freq"], test_dataloader=test_dataloader)
+    # Train renderer, save generated images, losses and model
+    trainer.train(dataloader, config["epochs"], save_dir=directory,
+                  save_freq=config["save_freq"], test_dataloader=test_dataloader)
 
-# Print best losses
-print("Model id: {}".format(config["id"]))
-print("Best train loss: {:.4f}".format(min(trainer.epoch_loss_history["total"])))
-print("Best validation loss: {:.4f}".format(min(trainer.val_loss_history["total"])))
+    # Print best losses
+    print("Model id: {}".format(config["id"]))
+    print("Best train loss: {:.4f}".format(min(trainer.epoch_loss_history["total"])))
+    print("Best validation loss: {:.4f}".format(min(trainer.val_loss_history["total"])))
+
+
+if __name__ == "__main__":
+    main()
